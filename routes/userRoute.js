4fd574bfabb7704e33d4e02149a8436fd711fada
jayant771 <jayantsingh771@gmail.com>
2025-03-30 19:15:52 +0530

    update

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const router = express.Router();
+const myapp = express.Router();
 
 const {registerUser,loginUser,currentUser} = require("../Controllers/userController");
 const validateToken = require("../middleware/validateTokenHandler");
 
-router.post("/register",registerUser);
+myapp.post("/register",registerUser);
 // router.post("/api/user/register",registerUser);
 
-router.post("/login",loginUser);
+myapp.post("/login",loginUser);
 
 
-router.get("/current", validateToken, currentUser);
+myapp.get("/current", validateToken, currentUser);
 
 module.exports = router; 
